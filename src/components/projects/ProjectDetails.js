0e32ca2edc2c0bbd536c1a172ee1237cc076703b
Luke-Draper <lukepdraper@gmail.com>
2018-10-26 22:38:15 -0500

    Project Displays

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,13 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ProjectDetails extends Component {
-	render() {
-		return (
-			<div className="project-details">
-				<h3>ProjectDetails</h3>
+const ProjectDetails = () => {
+	return (
+		<div className="container section ProjectDetails">
+			<div className="card z-depth-0">
+				<div className="card-content">
+					<span className="card-title">Project Title</span>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ex
+						quae enim soluta cum, consequuntur provident cupiditate tenetur sunt
+						dignissimos? Quidem delectus atque quaerat cupiditate quisquam sit
+						amet dolore ratione.
+					</p>
+				</div>
+				<div className="card-action grey lighten-4 grey-text">
+					<div>Posted by : Luke Draper</div>
+					<div>September 2nd, 17:00</div>
+				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default ProjectDetails;
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ProjectSummary extends Component {
-	render() {
-		return (
-			<div className="project-summary">
-				<h2>ProjectSummary</h2>
+const ProjectSummary = () => {
+	return (
+		<div className="card z-depth-0 project-summary">
+			<div className="card-content grey-text text-darken-3">
+				<span className="card-title">Project Title</span>
+				<p>Posted by Luke Draper</p>
+				<p className="grey-text">3rd September, 2018</p>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default ProjectSummary;
