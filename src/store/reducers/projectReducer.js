69acd9ee7c6d65351fec2d6ab04f20dc3d418a1d
Luke-Draper <lukepdraper@gmail.com>
2018-10-31 16:51:14 -0500

    Begin to add State Reducers

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,25 @@
-const initState = {};
+const initState = {
+	projects: [
+		{
+			id: "1",
+			title: "Project 1",
+			content:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore dolor porro quasi, hic dolorum ipsam, voluptatibus veritatis dicta nisi nostrum sint corporis. Corporis ad aperiam, itaque atque mollitia unde!"
+		},
+		{
+			id: "2",
+			title: "Project 2",
+			content:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore dolor porro quasi, hic dolorum ipsam, voluptatibus veritatis dicta nisi nostrum sint corporis. Corporis ad aperiam, itaque atque mollitia unde!"
+		},
+		{
+			id: "3",
+			title: "Project 3",
+			content:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore dolor porro quasi, hic dolorum ipsam, voluptatibus veritatis dicta nisi nostrum sint corporis. Corporis ad aperiam, itaque atque mollitia unde!"
+		}
+	]
+};
 
 const projectReducer = (state = initState, action) => {
 	return state;
